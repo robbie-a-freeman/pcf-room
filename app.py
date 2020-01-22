6eaa9740a5e99b0075ceb2a9ba466df22faa9d13
Robbie Freeman <robbie.a.freeman@gmail.com>
2020-01-22 15:20:28 -0500

    adding changeStatus and home functions

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-	return 'Hello, World!'
+isOpen = False
 
+@app.route('/')
+def home():
+    answer = '<p style="color:red;">not right now. Check back later!</p>'
+    if isOpen:
+        answer = '<p style="color:green;">yes it is! Stop on by!</p>'
+    return ' '.join(['<h1>','Is Spelman 43 open currently? The answer is:', answer, '</h1>'])
+
+@app.route('/changeStatus')
+def changeStatus():
+    global isOpen
+    current = not isOpen
+    isOpen = current
+    return 'Successfully changed status'
+    
 if __name__ == "__main__":
     app.run()
